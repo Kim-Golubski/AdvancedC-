a529376f683cb64868dd8930114807c3a92ef1d1
Kim-Golubski <78122291+Kim-Golubski@users.noreply.github.com>
2022-06-08 16:51:09 -0500

    Create main.cpp

---
+++ Asgn16/main.cpp
@@ -0,0 +1,13 @@
+#include "proj16.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "BITWISE OPERATIONS\n\n";
+	bitwise();
+	cout << "\nCHARACTER HANDLING\n\n";
+	characterHandling();
+	cout << "\n\nC-STRING FUNCTIONS\n\n";
+	cStringManip();
+}
