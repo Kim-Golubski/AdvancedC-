3a96e9a38f8ceec324fbbd2a56cc8962d2b462ec
Kim-Golubski <78122291+Kim-Golubski@users.noreply.github.com>
2022-06-08 16:49:37 -0500

    Create main.cpp

---
+++ Asgn15/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "String.h"
+using namespace std;
+
+
+
+int main()
+{
+	createStrings();
+	concatStrings();
+	compareStrings();
+	demoIterators();
+	demoIO();
+}
